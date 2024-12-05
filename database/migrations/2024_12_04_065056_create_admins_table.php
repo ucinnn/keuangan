c330a70aa3c733e6864d5ca7467212e7a3f5745c
FarhanFuady090 <muhammadfarhanfuady@gmail.com>
2024-12-06 00:38:20 +0700

    update seputar login

--- database/migrations/2024_12_04_065056_create_admins_table.php
+++ database/migrations/2024_12_04_065056_create_admins_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
